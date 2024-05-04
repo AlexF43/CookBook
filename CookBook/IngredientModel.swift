8330948fe29b8662c562a10741c60446f39ef3aa
rhimcc <rio.mccormack25@gmail.com>
2024-05-04 12:14:28 +1000

    beginning ingredient adding

--- CookBook/IngredientModel.swift
+++ CookBook/IngredientModel.swift
@@ -8,18 +8,20 @@
 import Foundation
 import SwiftData
 
-struct Ingredient: Codable {    
+struct Ingredient: Codable, Identifiable {
     
-    
+    var id: UUID
     var name: String
     var amount: Double
     var unit: String
     var descName: String
     
     init(name: String, amount: Double, unit: String, descName: String) {
+        self.id = UUID()
         self.name = name
         self.amount = amount
         self.unit = unit
         self.descName = descName
     }
+    
 }
