b3387928aabda3971c6d372bc8bd33515de12dec
rhimcc <rio.mccormack25@gmail.com>
2024-05-11 12:09:58 +1000

    made task bar

---
+++ CookBook/Views/FeelingLuckyView.swift
@@ -0,0 +1,18 @@
+//
+//  FeelingLuckyView.swift
+//  CookBook
+//
+//  Created by Rhianna McCormack on 11/5/2024.
+//
+// NOTE: this will be implemented in HomeContentView when we can get Anya's code
+import SwiftUI
+
+struct FeelingLuckyView: View {
+    var body: some View {
+        Text("Feeling Lucky?")
+    }
+}
+
+#Preview {
+    FeelingLuckyView()
+}
